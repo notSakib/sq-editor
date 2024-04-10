59519aff5044b479a85097b851aaf4709dfaf18f
sakibProcol <sakib.khan@procol.in>
2024-04-10 21:56:28 +0530

    added check for deployment

--- src/app/(feature)/query/page.tsx
+++ src/app/(feature)/query/page.tsx
@@ -3,6 +3,9 @@ import SqlEditor from "./sqlEditor";
 import { getSavedQueries } from "@/lib/api";
 
 const Query = async () => {
+  if (!process.env.NEXT_PUBLIC_API_URL) {
+    return null;
+  }
   const savedQueries = await getSavedQueries();
   return <SqlEditor savedQueriesInitialData={savedQueries} />;
 };
