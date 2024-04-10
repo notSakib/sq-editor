896aec5662c5616ad87e9f1b135202ddaac7d65a
sakibProcol <sakib.khan@procol.in>
2024-04-10 20:19:25 +0530

    env changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3002/api";
+const BASE_URL = "https://" + process.env.VERCEL_URL + "/";
 export const getQueryData = async () => {
   const res = await fetch(`${BASE_URL}/getData`, {
     method: "GET",
