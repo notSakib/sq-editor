9d51b6b2a260c09b42a3ee97aacbd466a869cc8d
sakibProcol <sakib.khan@procol.in>
2024-04-12 00:34:56 +0530

    checking metric without editor

--- src/components/common/sqlEditor.tsx
+++ src/components/common/sqlEditor.tsx
@@ -1,15 +1,6 @@
-import CodeEditor from "@uiw/react-textarea-code-editor";
+// import CodeEditor from "@uiw/react-textarea-code-editor";
 function SqlEditor({ setValue, value }: any) {
-  return (
-    <CodeEditor
-      value={value}
-      language="sql"
-      placeholder=""
-      onChange={(evn: any) => setValue(evn.target.value)}
-      padding={15}
-      className="h-full w-full rounded-[8px] bg-primary-foreground"
-    />
-  );
+  return <></>;
 }
 
 export default SqlEditor;
