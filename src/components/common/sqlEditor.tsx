a5a8e188dfd465b0f8bf8c40c6ac3a3aa10b29c0
sakibProcol <sakib.khan@procol.in>
2024-04-12 01:39:22 +0530

    changed code editor

--- src/components/common/sqlEditor.tsx
+++ src/components/common/sqlEditor.tsx
@@ -1,6 +1,31 @@
-// import CodeEditor from "@uiw/react-textarea-code-editor";
-function SqlEditor({ setValue, value }: any) {
-  return <></>;
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-language_tools";
+
+function SqlEditor({ setValue, value }: { setValue: any; value: string }) {
+  return (
+    <AceEditor
+      mode="mysql"
+      value={value}
+      onChange={setValue}
+      name="CODE_EDITOR"
+      fontSize={14}
+      lineHeight={20}
+      showGutter={true}
+      highlightActiveLine={false}
+      showPrintMargin={false}
+      editorProps={{ $blockScrolling: true }}
+      setOptions={{
+        enableBasicAutocompletion: true,
+        enableLiveAutocompletion: true,
+        enableSnippets: true,
+        showLineNumbers: true,
+        tabSize: 0,
+      }}
+    />
+  );
 }
 
 export default SqlEditor;
